2e34c0071ed6f12edb946620c86ade5f0b58290a
oneextrafact <1extrafact@gmail.com>
2022-08-20 18:27:08 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ingest various Long Island news sources to assist with knowledge discovery. ',
+    author='Dave Gallagher',
+    license='MIT',
+)
