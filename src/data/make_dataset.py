59108dbd94d4b351e2d10f42c21af4bf74d9ce76
oneextrafact <1extrafact@gmail.com>
2022-08-27 17:55:19 +0000

    Read sitemap and download letters.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
+import click
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
